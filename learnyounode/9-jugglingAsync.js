40f5bcf680ba281bbbf3de185ccdecd3959ac148
GSoster <guilhermesosters@gmail.com>
2016-06-14 15:01:02 -0300

    learnyounode: completed challange 9 of 13 - Juggling Async

--- learnyounode/9-jugglingAsync.js
+++ learnyounode/9-jugglingAsync.js
@@ -1,28 +1,35 @@
+//exercise 9 of 13
 var http = require('http');
 var counter = 0;
-var objList = [];
+var resp = [];
+
+function checkCounter () {
+  if (counter === 3) {
+    for (var i = 0; i < counter; i++) {
+      console.log(resp[i]);
+    }
+  }
+};
+
+/* it is necessary because of the async behaviour, if the http.get were
+* made directly inside the for loop the http.get would still be waiting for
+* an answer and the loop would already finished.
+*/
+function getResponse(index){
+  http.get(process.argv[2 + index], function (response) {
+    response.setEncoding('utf8');
+    var fullData = '';
+    response.on('data', function(chuck){
+      fullData += chuck;
+    });
+    response.on('end', function(){
+        resp[index] = fullData;
+        counter++;
+        checkCounter();
+    });
+  });
+};
 
 for (var i = 0; i < 3; i++) {
-  objList[i] = {url : process.argv[i + 2], fullData : '', checkCounter : function () {
-    //console.log("checkCounter: " + counter);
-    if (counter === 2) {
-      for (var j = 0; j < counter; j++) {
-        console.log(objList[j].fullData);
-      }
-    }
-  }};
+  getResponse(i);
 }
-
-function callback (response) {
-  var fullData = "";
-  response.setEncoding('utf8');
-  response.on('data', function(data){ fullData += data;});
-  response.on('end', function(data){
-    fullData += data;
-    objList[counter].fullData = fullData;
-    objList[counter].checkCounter();
-    counter++;
-  });
-}
-
-objList.map(function (obj) {http.get(obj.url, callback);});
