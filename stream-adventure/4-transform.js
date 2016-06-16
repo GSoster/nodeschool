e1b633e6351692b63ef5e165e9b041b6528ea47f
GSoster <guilhermesosters@gmail.com>
2016-06-16 14:57:46 -0300

    stream-adventure: modifying exercise 4 to test functions behaviour.

--- stream-adventure/4-transform.js
+++ stream-adventure/4-transform.js
@@ -9,7 +9,11 @@ function write (chuck, enconding, next) {
 function end (done) {
   done();
 };
-
-var stream = through(write, end);
+//both 'write' and 'end' are optional functions.
+//default behaviour for write: pass the chucks of data forward.
+//default behaviour for end: pass NULL and end stream.
+//any of the below will work and pass the Exercise:
+//var stream = through(write, end);
+var stream = through(write);
 
 process.stdin.pipe(stream).pipe(process.stdout);
